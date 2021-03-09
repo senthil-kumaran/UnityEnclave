ed4a7b63ac80f4aa8c70d69de69fbdecdf06a2ff
Senthil Kumaran <kumaranfizz@gmail.com>
2021-03-09 13:17:27 +0530

    Tmp fix: Cors for all

--- route/viewRouter.js
+++ route/viewRouter.js
@@ -9,12 +9,13 @@ const router = express.Router()
 
 router.use(authController.isLoggedIn)
 
-//CORS
-//ACCESS-CONTROL-ALLOW-ORIGIN
-router.get('/', cors({
-    origin: ['https://www.google.com/maps', 'https://www.youtube.com/']
-}), viewController.overview)
+// //CORS
+// //ACCESS-CONTROL-ALLOW-ORIGIN
+// router.get('/', cors({
+//     origin: ['https://www.google.com/maps', 'https://www.youtube.com/']
+// }), viewController.overview)
 
+router.get('/', viewController.overview)
 router.get('/signup', viewController.signup) 
 router.get('/login', viewController.login) 
 router.get('/forget-password', viewController.forgotPassword) 
